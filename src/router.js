9f8e16f112a46ddd6f7a24f937e735b0b19766cd
daraakitoyemru <dakit711@mtroyal.ca>
2025-02-24 13:37:02 -0700

    added files

--- src/router.js
+++ src/router.js
@@ -1,7 +1,6 @@
 const { createClient } = require("@supabase/supabase-js");
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_ANON_KEY;
-
 const db = createClient(supabaseUrl, supabaseKey);
 
 const eraSql = '"eraId", "eraName", "eraYears"';
